fda43298083ed36b7ccade262cde8f33a13976f9
Glen Pit-Pladdy <glenpp@users.noreply.github.com>
2015-06-27 18:09:20 +0100

    Used full (verbose) package / namespace syntax for classifier

--- classifiercli.py
+++ classifiercli.py
@@ -21,14 +21,14 @@
 #TODO See: https://www.pitt-pladdy.com/blog/_20111229-214727_0000_Bayesian_Classifier_Classes_for_Perl_and_PHP/
 #
 
-from classifier import classifier
+import classifier
 import sqlite3
 import sys
 import re
 
 if len(sys.argv) >= 5 and sys.argv[2] == 'classify' and sys.argv[3].isdigit() and sys.argv[4].isdigit():
 	db = sqlite3.connect ( sys.argv[1] )
-	classifier = classifier ( db, sys.stdin.read() )
+	classifier = classifier.classifier ( db, sys.stdin.read() )
 	classifier.unbiased = True
 	clases = [ int(x) for x in sys.argv[3:] ]
 	prob = classifier.classify ( clases )
@@ -36,19 +36,19 @@ if len(sys.argv) >= 5 and sys.argv[2] == 'classify' and sys.argv[3].isdigit() an
 		print "class%d: %f" % ( clas, prob[clas] )
 elif len ( sys.argv ) >= 4 and len ( sys.argv ) <= 5 and sys.argv[2] == 'teach' and sys.argv[3].isdigit():
 	db = sqlite3.connect ( sys.argv[1] )
-	classifier = classifier ( db, sys.stdin.read() )
+	classifier = classifier.classifier ( db, sys.stdin.read() )
 	if len ( sys.argv ) >= 4:
 		classifier.teach ( int(sys.argv[3]) )
 	else:
 		classifier.teach ( int(sys.argv[3]), float ( sys.argv[4] ) )
 elif len ( sys.argv ) == 2 and sys.argv[2] == 'updatequality':
 	db = sqlite3.connect ( sys.argv[1] )
-	classifier = classifier ( db, '' )
+	classifier = classifier.classifier ( db, '' )
 	classifier.unbiased = True
 	classifier.updatequality ()
 elif len ( sys.argv ) == 2 and sys.argv[2] == 'degrade' and re.match ( '^\d+(\.\d+)?$', sys.argv[3] ):
 	db = sqlite3.connect ( sys.argv[1] )
-	classifier = classifier ( db, '' )
+	classifier = classifier.classifier ( db, '' )
 	classifier.degrade ( sys.argv[3] )
 else:
 	sys.exit ( "Usage: %s <sqlite file> [teach <clasid> [weighting]|clasify <clasid> <clasid> [clasid] [...]|updatequality|degrade <factor>]\n\ttext on STDIN" % sys.argv[0] )
